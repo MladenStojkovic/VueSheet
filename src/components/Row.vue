328a0b0ba8397c182943ee3b685f4382177297b5
Mladen_Stojkovic <mladen.stojkovic@badin.rs>
2021-09-14 09:18:14 +0200

    init

---
+++ src/components/Row.vue
@@ -0,0 +1,21 @@
+<template>
+	<tr>
+		<td v-for="(cell, index) in row" :key="index"> {{row[cell]}} </td>
+		<td>{{row.phone}}</td>
+		<td>{{row.activity}}</td>
+		<td>{{row.city}}</td>
+		{{ row }}
+	</tr>
+</template>
+
+<script>
+	export default {
+		name: "Row",
+		props: ["row"],
+	}
+	
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Sheet.vue
@@ -0,0 +1,71 @@
+<template>
+	<div class="container-fluid">
+		<div class="row">
+			<main role="main" class="col-md-12 ml-sm-auto col-lg-12 pt-3 px-4">
+				<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 ">
+					<h2>VueSheet List</h2>
+					<div class="btn-toolbar mb-2 mb-md-0">
+						<a 	href="https://docs.google.com/spreadsheets/d/1lei8ZdcTPPEP3DXpTMZHdGLCQyCr63adgJWL1LCe2zI/edit?usp=sharing" 
+							class="btn btn-sm btn-outline-secondary"
+							target="_blank">
+						View Google Sheet
+						</a>
+					</div>
+				</div>
+				<div class="table-responsive">
+				<table class="table table-striped ">
+					<thead>
+					<tr>
+						<th v-for="(title, index) of rows[0]" :key="index"> {{title}} </th>
+					</tr>
+					</thead>
+					
+					<tbody>
+						<Row :key="row.id" v-for="row in rows" :row="row" />
+					</tbody>
+				</table>
+				</div>
+			</main>
+		</div>
+	</div>
+</template>
+ 
+<script>
+import Row from '@/components/Row.vue';
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const creds = require('@/client_secret.json');
+	export default {
+		name: "Sheet",
+		components: {
+			Row
+		},
+		props: ["sheet"],
+		data() {
+			return {
+				rows: [],
+				loading: true,
+			}
+		},
+		methods:{
+			async accessSpreadSheet() {
+				const doc = new GoogleSpreadsheet('1lei8ZdcTPPEP3DXpTMZHdGLCQyCr63adgJWL1LCe2zI');
+				await doc.useServiceAccountAuth(creds);
+				await doc.loadInfo(); 
+				const sheet = doc.sheetsByIndex[0];
+				const  rows = await sheet.getRows({
+					offset: -1
+				})
+				this.rows = rows;
+				console.log(this.rows)
+			}
+		},
+		created() {
+			this.accessSpreadSheet();
+		}
+		
+	}
+</script>
+
+<style scoped>
+
+</style>
